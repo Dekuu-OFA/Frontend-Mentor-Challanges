26226c5f8d3b8473c345349737a0ac7467d4a5ef
Dekuu-OFA <82962364+Dekuu-OFA@users.noreply.github.com>
2021-07-05 18:22:57 +0530

    Delete base-apparel-coming-soon-master directory

--- base-apparel-coming-soon-master/index.js
+++
@@ -1,7 +0,0 @@
-$(window).resize(function() {
-  if ($(window).width() <= 768) {
-    $('.second').remove().insertAfter($('.third'));
-  } else {
-    $('.third').remove().insertBefore($('.second'));
-  }
-})
